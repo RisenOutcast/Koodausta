d3d3ff41c224ea800ad71af7f48e9c58cc8473cb
RisenOutcast <43453346+RisenOutcast@users.noreply.github.com>
2018-09-20 23:12:24 +0300

    Add files via upload

---
+++ Korttipeli/Vuorot/TaisteluPhase.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RO
+{
+    [CreateAssetMenu(menuName = "Vuorot/PelaajaTaisteluPhase")]
+    public class TaisteluPhase : Phase
+    {
+        public override bool IsComplete()
+        {
+            if (forceExit)
+            {
+                forceExit = false;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public override void OnEndPhase()
+        {
+            if (isInit)
+            {
+                Settings.peliSäätäjä.SetState(null);
+                isInit = false;
+            }
+        }
+
+        public override void OnStartPhase()
+        {
+            if (!isInit)
+            {
+                Settings.peliSäätäjä.SetState(null);
+                Settings.peliSäätäjä.onPhaseChanged.Raise();
+                isInit = true;
+            }
+        }
+    }
+}
