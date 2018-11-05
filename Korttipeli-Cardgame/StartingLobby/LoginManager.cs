efae54817a77fbedc94db137322c50065c0d0ced
Metso <43453346+RisenOutcast@users.noreply.github.com>
2018-11-05 20:08:42 +0200

    Add files via upload

---
+++ Korttipeli-Cardgame/StartingLobby/LoginManager.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace RO.Login
+{
+    public class LoginManager : MonoBehaviour
+    {
+
+        public TMP_InputField UsernameUI;
+        public TMP_InputField PasswordUI;
+        public TMP_InputField RegisterUsernameUI;
+        public TMP_InputField RegisterPasswordUI;
+        public TMP_InputField ReEnterPasswordUI;
+        public TMP_InputField EmailAddressUI;
+
+        public string UsernameString;
+        public string PasswordString;
+        public string RegisterUsernameString;
+        public string RegisterPasswordString;
+        public string ReEnterPasswordString;
+        public string EmailAddressString;
+
+        public Toggle SaveUsernameToggle;
+
+        public bool PasswordsMatch;
+        public bool FieldsAreGiven;
+
+        public TMP_Text Alerts;
+        public string AlertText;
+        private Color32 AlertColor; 
+
+        public GameObject RegisterTab;
+
+        public string savedUsername;
+        public bool Connecting;
+
+        private string OldAlert;
+
+        void Awake()
+        {
+            savedUsername = PlayerPrefs.GetString("Username");
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            Connecting = false;
+            if(savedUsername == "")
+            {
+                SaveUsernameToggle.isOn = false;
+            }
+            else
+            {
+                UsernameUI.text = savedUsername;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            UsernameString = UsernameUI.text;
+            PasswordString = PasswordUI.text;
+
+            RegisterUsernameString = RegisterUsernameUI.text;
+            RegisterPasswordString = RegisterPasswordUI.text;
+            ReEnterPasswordString = ReEnterPasswordUI.text;
+            EmailAddressString = EmailAddressUI.text;
+
+            //Changing Alerts Colour
+            //(WHITE) Alerts.color = new Color32(255, 255, 255, 255);
+            //(RED) Alerts.color = new Color32(255, 0, 0, 255);
+            //(GREEN) Alerts.color = new Color32(0, 255, 23, 255);
+            //(BLUE) Alerts.color = new Color32(0, 160, 255, 255);
+
+            if (RegisterPasswordString == ReEnterPasswordString)
+            {
+                PasswordsMatch = true;
+            }
+            else
+            {
+                PasswordsMatch = false;
+            }
+
+            if(RegisterUsernameString != "" && RegisterPasswordString != "" && ReEnterPasswordString != "" && EmailAddressString != "")
+            {
+                FieldsAreGiven = true;
+            }
+            else
+            {
+                FieldsAreGiven = false;
+            }
+
+            if(PasswordsMatch == false && RegisterPasswordString != "" || PasswordsMatch == false && ReEnterPasswordString != "")
+            {
+                AlertText = "Passwords do not match";
+                AlertColor = new Color32(255, 0, 0, 255);
+            }
+
+            if(PasswordsMatch == true && RegisterTab.activeSelf == true)
+            {
+                AlertText = "";
+                AlertColor = new Color32(255, 255, 255, 255);
+            }
+        }
+
+        void LateUpdate()
+        {
+            SetAlert();    
+        }
+
+        public void LogginIn()
+        {
+            if (SaveUsernameToggle.isOn == true)
+            {
+                SaveUsername();
+                Debug.Log("Saving...");
+            }
+            else
+            {
+                DeleteUsername();
+                Debug.Log("Deleting...");
+            }
+            AlertText = "Connecting";
+            AlertColor = new Color32(0, 160, 255, 255);
+            Connecting = true;
+            StartCoroutine(RollDotsOnText()); //Jos tulee joku virhe esim. pelaajaa ei löydy niin pistää vaan StopCoroutine...
+        }
+
+        void GetSavedUsername()
+        {
+            UsernameUI.text = PlayerPrefs.GetString("Username", null);
+        }
+
+        void SaveUsername()
+        {
+            PlayerPrefs.SetString("Username", UsernameString);
+        }
+
+        void DeleteUsername()
+        {
+            PlayerPrefs.DeleteKey("Username");
+        }
+
+        void SetAlert()
+        {
+            Alerts.text = AlertText;
+            Alerts.color = AlertColor;
+        }
+
+        public void ClearAlerts()
+        {
+            Alerts.text = "";
+            Alerts.color = new Color32(255, 255, 255, 255);
+        }
+
+        IEnumerator RollDotsOnText()
+        {
+            OldAlert = AlertText;
+            yield return new WaitForSeconds(0.2F);
+            if (Connecting == true)
+            {
+                AlertText = (AlertText + ".");
+                SetAlert();
+            }
+            yield return new WaitForSeconds(0.2F);
+            if (Connecting == true)
+            {
+                AlertText = (AlertText + ".");
+                SetAlert();
+            }
+            yield return new WaitForSeconds(0.2F);
+            if (Connecting == true)
+            {
+                AlertText = (AlertText + ".");
+                SetAlert();
+            }
+            yield return new WaitForSeconds(0.2F);
+            if(Connecting == true)
+            {
+                AlertText = OldAlert;
+                StartCoroutine(RollDotsOnText());
+            }
+        }
+    }
+}
---
+++ Korttipeli-Cardgame/Säätäjät/PeliSäätäjä.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RO.GameStates;
+
+namespace RO
+{
+    public class PeliSäätäjä : MonoBehaviour
+    {
+        public PlayerHolder[] all_players;
+        public PlayerHolder currentPlayer;
+        public CardHolders playerOneHolder;
+        public CardHolders otherPlayersHolder;
+        public State currentState;
+        public GameObject korttiPrefab;
+
+        public int vuoroIndex;
+        public Vuoro[] vuorot;
+        public RO.PeliEventit onTurnChanged;
+        public RO.PeliEventit onPhaseChanged;
+        public RO.StringVariable turnText;
+
+        private void Start()
+        {
+            Settings.peliSäätäjä = this;
+
+            SetupPlayers();
+
+            LuoAlotusKortit();
+
+            turnText.value = vuorot[vuoroIndex].player.username;
+            onTurnChanged.Raise();
+        }
+
+        void LuoAlotusKortit()
+        {
+            ResurssiSäätäjä rs = Settings.GetResurssiSäätäjä();
+
+            for (int p = 0; p < all_players.Length; p++)
+            {
+                for (int i = 0; i < all_players[p].alotusKortit.Length; i++)
+                {
+                    GameObject go = Instantiate(korttiPrefab) as GameObject;
+                    KortinAsentaja a = go.GetComponent<KortinAsentaja>();
+                    a.LataaKortti(rs.HaeKorttiInstanssi(all_players[p].alotusKortit[i]));
+                    KorttiInstanssi inst = go.GetComponent<KorttiInstanssi>();
+                    inst.currentLogic = all_players[p].käsilogiikka;
+                    Settings.SetParentForCard(go.transform, all_players[p].currentHolder.käsiGridi.value);
+                    all_players[p].kortitKädes.Add(inst);
+                }
+
+                Settings.RegisterEvent("Starting cards have been drawn for " + all_players[p].username, all_players[p].playerColor);
+            }
+        }
+
+        void SetupPlayers()
+        {
+            foreach (PlayerHolder p in all_players)
+            {
+                if (p.isHumanPlayer)
+                {
+                    p.currentHolder = playerOneHolder;
+                }
+                else
+                {
+                    p.currentHolder = otherPlayersHolder;
+                }
+            }
+        }
+
+        private void Update()
+        {
+            bool isComplete = vuorot[vuoroIndex].Execute();
+
+            if (isComplete)
+            {
+
+                vuoroIndex++;
+                if(vuoroIndex > vuorot.Length - 1)
+                {
+                    vuoroIndex = 0;
+                }
+
+                turnText.value = vuorot[vuoroIndex].player.username;
+                onTurnChanged.Raise();
+            }
+
+            if(currentState != null)
+            currentState.Tick(Time.deltaTime);            
+        }
+
+        public void SetState(State state)
+        {
+            currentState = state;
+        }
+
+        public void EndCurrentPhase()
+        {
+            Settings.RegisterEvent(vuorot[vuoroIndex].name + " finished", currentPlayer.playerColor);
+
+            vuorot[vuoroIndex].EndCurrentPhase();
+        }
+    }
+}
